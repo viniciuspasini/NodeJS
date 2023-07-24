b5450f6b26faea264b93ba4d7b3062911197dfb0
Vinicius Pasini <vinipasini@hotmail.com>
2023-07-24 23:07:28 +0200

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('olá mundo');
+import chalk from 'chalk';
+import fs from 'fs';
+
+function trataErro(erro){
+    throw new Error(chalk.red(erro.code, 'Erro'))
+}
+
+function pegaArquivo(path){
+    fs.readFile(path, 'utf-8', (erro, texto)=>{
+        if(erro){
+            trataErro(erro)
+        }
+        console.log(chalk.green(texto))
+    })
+}
+
+pegaArquivo('./arquivos/texto.md')
